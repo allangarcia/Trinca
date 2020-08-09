359ebdb6b79543d69aba52125ee0ffe0b03c408c
Allan Garcia <allan.garcia@gmail.com>
2020-08-09 10:54:13 +0100

    Initial Commit

---
+++ Trinca/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trinca
+//
+//  Created by Allan Garcia on 09/08/2020.
+//  Copyright © 2020 Allan Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
